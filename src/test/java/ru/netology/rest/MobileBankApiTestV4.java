1da00d5cd2336cabd37cf0b974f952f14c7ffdc4
Natalya Storchilova <nata@Storcilovas-MacBook-Pro.local>
2020-09-09 10:39:47 +0300

    The scheme is modified incorrectly

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -14,11 +14,12 @@ class MobileBankApiTestV4 {
                 .baseUri("http://localhost:9999/api/v1")
                 // Выполняемые действия
                 .when()
-                .get("/demo/accounts")
+                   .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(200)
-                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                   .statusCode(200)
+                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+                   .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
         ;
     }
 }
